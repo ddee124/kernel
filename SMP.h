0955f7d074b65ea8747f7f52f0ce8550da79a91e
ddd124 <124370218+ddee124@users.noreply.github.com>
2026-06-24 09:59:50 +0800

    Delete SMP.h

--- SMP.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SMP_H__
-#define __SMP_H__
-#include "spinlock.h"
-extern unsigned char _APU_boot_start[];
-extern unsigned char _APU_boot_end[];
-extern void boot_ap(unsigned int bsp_id);
-struct spinlock_T SMP_lock;
-#endif
